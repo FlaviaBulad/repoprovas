5aed1da83c784f99d03a272bdb21704d7712524d
Flavia <flavia.bulad@gmail.com>
2022-09-16 16:01:18 -0300

    feat: add error handler middleware

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express, { json } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
+import { errorHandler } from './middlewares/errorHandler';
 
 const app = express();
 app.use(cors(), json());
 // app.use(router);
-// app.use(errorHandler);
+app.use(errorHandler);
 
 export default app;
